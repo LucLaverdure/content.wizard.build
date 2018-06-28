1441b18c864d15d4895d99dd99fb706f97d0be55
contact@luclaverdure.com <contact@luclaverdure.com>
2018-06-28 06:32:09 -0400

    Featuring IMAGES!

--- includes/helper.functions.php
+++ includes/helper.functions.php
@@ -414,13 +414,13 @@ function parseAfterOp($html, $op, $opeq) {
 			// nothing to do here
 			break;
 		case "imgsrc":
-
 			$doc = phpQuery::newDocument('<div>'.$html.'</div>'); 
 			$code = $doc->find("img");
 			$img = "";
 			foreach (pq($code) as $k => $thisf) {
 				$to_push = pq($thisf)->html();
 				$img = pq($thisf)->attr("src");
+				$img = str_replace("//", "http://", $img);
 				$html = $img;
 			}
 			break;
@@ -633,7 +633,7 @@ function runmap($offset, $mapCount, $json_config) {
 											$meta[$kname] = $val;
 										}
 									}
-
+									
 									// create product category if doesn`t exist
 									if (isset($my_post["product_cat"])) {
 										wp_insert_term(
@@ -663,6 +663,9 @@ function runmap($offset, $mapCount, $json_config) {
 										update_post_meta(get_the_ID(), $mk, $mv);
 									}
 									
+									if (isset($my_post["thumbnail"])) {
+										add_image(get_the_ID(), $my_post["thumbnail"], basename($my_post["thumbnail"]));
+									}
 									
 								endwhile;
 								wp_reset_postdata();
@@ -712,10 +715,13 @@ function runmap($offset, $mapCount, $json_config) {
 								foreach($meta as $mk => $mv) {
 									update_post_meta($pid , $mk, $mv);
 								}
-
-									
-							}
 							
+								if (isset($my_post["thumbnail"])) {								
+									add_image($pid, $my_post["thumbnail"], basename($my_post["thumbnail"]));
+								}
+								
+							}
+
 						} // valid
 						
 					} // containers
@@ -728,6 +734,57 @@ function runmap($offset, $mapCount, $json_config) {
 			echo "EOQ";
 		}
 	}
+}
+
+function add_image($post_id, $image_url, $image_name) {
+// Add Featured Image to Post
+    //$image_url        = 'http://s.wordpress.org/style/images/wp-header-logo.png'; // Define the image URL here
+    //$image_name       = 'wp-header-logo.png';
+	$image_url = strtok($image_url, '?');
+	$image_name = strtok($image_name, '?');
+    $upload_dir       = wp_upload_dir(); // Set upload folder
+    $image_data       = file_get_contents($image_url); // Get image data
+    $unique_file_name = wp_unique_filename( $upload_dir['path'], $image_name ); // Generate unique name
+    $filename         = basename( $unique_file_name ); // Create image file name
+
+    // Check folder permission and define file location
+    if( wp_mkdir_p( $upload_dir['path'] ) ) {
+        $file = $upload_dir['path'] . '/' . $filename;
+    } else {
+        $file = $upload_dir['basedir'] . '/' . $filename;
+    }
+
+    // Create the image  file on the server
+    file_put_contents( $file, $image_data );
+
+    // Check image file type
+    $wp_filetype = wp_check_filetype( $filename, null );
+
+    // Set attachment data
+    $attachment = array(
+        'post_mime_type' => $wp_filetype['type'],
+        'post_title'     => sanitize_file_name( $filename ),
+        'post_content'   => '',
+		'guid'			 => $unique_file_name,
+        'post_status'    => 'inherit'
+    );
+
+    // Create the attachment
+    $attach_id = wp_insert_attachment( $attachment, $file, $post_id );
+
+    // Include image.php
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+    // Define attachment metadata
+    $attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+
+    // Assign metadata to attachment
+    wp_update_attachment_metadata( $attach_id, $attach_data );
+
+    // And finally assign featured image to post
+    set_post_thumbnail( $post_id, $attach_id );
+	
+	update_post_meta($post_id, '_thumbnail_id', $attach_id);
 }
 
 ?>
